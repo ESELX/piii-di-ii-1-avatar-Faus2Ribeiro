f463d5a1f1044f85a2c28ddb36205d48e6962f66
faus2ribeiro <Faus2Ribeiro@users.noreply.github.com>
2019-11-06 18:07:31 +0000

    Update ex1.js

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -12,8 +12,35 @@ function setup() {
 
 }
 
-function draw() {
-  background(220);
-
-
+function setup() {
+  createCanvas(400, 400);
 }
+
+function draw() {
+  background(220)
+
+  fill(45);
+  quad(130,165,275,165,250,350,155,350);
+  fill(255, 216, 201)
+  triangle (155,120,175,170,175,120);
+
+  triangle (220,120,225,170,246,120);
+
+  rect(175,130,50,40);
+
+  ellipse(200,100,100,100);
+  fill(255)
+  triangle(140,85,175,120,200,90);
+  fill(255)
+  triangle(260,85,220,120,200,90);
+  fill(25)
+  circle (175,100,20);
+  fill(25)
+  circle (222,100,20);
+
+  quad (120,135,150,160,180,350,155,350);
+
+  quad (280,135,250,160,225,350,250,350);
+
+
+  }
